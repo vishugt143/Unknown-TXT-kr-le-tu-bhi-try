2e9d97ddb7116cd83a6b504ec84abf54d0d022b0
ojhaakkio <157199909+ojhaakkio@users.noreply.github.com>
2024-01-23 14:06:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) ACE 
-
 import os
 
-class Config(object):
-    # get a token from @BotFather
-    pass
-    """
-    API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    AUTH_USERS= int(os.environ.get("AUTH_USERS", 0))
+API_ID = API_ID = 20346550
 
+API_HASH = os.environ.get("API_HASH", "bc79c3bea7a626887bdc0871eecf0327")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6887502068:AAEpQVEnZsgaOPg35_77654QzxPwAPrYahs")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 6381511858))
+
+LOG = -1002000067767
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "5665231556").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+ADMINS.append(OWNER)
